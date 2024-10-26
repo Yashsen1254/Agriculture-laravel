dceda1c17e3ed9c54e7fa9e6e9ec46cf742c7ca0
Yash Sen <yashsen0402@gmail.com>
2024-10-26 13:59:08 +0530

    Routes Created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\Frontend\Pages\IndexController;
+use App\Http\Controllers\Frontend\Pages\AboutController;
+use App\Http\Controllers\Frontend\Pages\CartController;
+use App\Http\Controllers\Frontend\Pages\CheckoutController;
+use App\Http\Controllers\Frontend\Pages\ContactController;
+use App\Http\Controllers\Frontend\Pages\ShopController;
+use App\Http\Controllers\Frontend\Pages\WishlistController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/shop', [ShopController::class, 'index']);
+Route::get('/cart', [CartController::class, 'index']);
+Route::get('/checkout', [CheckoutController::class, 'index']);
+Route::get('/wishlist', [WishlistController::class, 'index']);
